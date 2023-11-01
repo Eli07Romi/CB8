746691ebf90e8b1e847ec0362e8407c28241ea44
Eli <148492106+Eli07Romi@users.noreply.github.com>
2023-11-01 14:10:59 +0100

    Esercizio 01-11-23

---
+++ 01-11-23/script.js
@@ -0,0 +1,17 @@
+const Animals = [
+  "Horse",
+  "Cat",
+  "Dog",
+  "Tiger",
+  "Leon",
+  "Butterfly",
+  "Duck",
+  "Donkey",
+  "Bird",
+];
+
+console.log("Animals:");
+
+for (let i = 0; i <= Animals.length - 1; i++) {
+  console.log(Animals[i]);
+}
