c8414e3fa82c7cbed57db92943ea285c23f43a39
Eli <148492106+Eli07Romi@users.noreply.github.com>
2023-11-01 20:05:07 +0100

    01-11-23
    Esercizio update

--- 01-11-23/script.js
+++ 01-11-23/script.js
@@ -12,6 +12,22 @@ const Animals = [
 
 console.log("Animals:");
 
+// parte da 0:
+
 for (let i = 0; i <= Animals.length - 1; i++) {
   console.log(Animals[i]);
+}
+
+console.log("Animals:");
+
+// parte da 1
+for (let i = 1; i <= Animals.length - 1; i++) {
+  console.log(Animals[i]);
+}
+
+// metto gli indici accanto
+
+console.log("Animals:");
+for (let i = 0; i < Animals.length; i++) {
+  console.log(i + 1 + ": " + Animals[i]);
 }
